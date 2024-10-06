36c4d4da718a1a477b19aed87d511c53e8f4934c
richardbsam <richardbonifacesam@gmail.com>
2024-10-06 07:28:45 +0100

    error handling

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts") 
 const env = require("dotenv").config()
 const app = express()
+const utilities = require("./utilities/index")
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute");
@@ -31,6 +32,27 @@ app.get("/", baseController.buildHome)
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
   
 /* ***********************
  * Local Server Information
