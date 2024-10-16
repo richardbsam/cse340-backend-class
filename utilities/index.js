97499aec714ba3c666fab3e43152b352c6582df5
richardbsam <richardbonifacesam@gmail.com>
2024-10-16 08:40:38 +0100

    w03

--- utilities/index.js
+++ utilities/index.js
@@ -108,6 +108,12 @@ Util.buildClassificationGrid = async function(data){
 }
 
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
 
 module.exports = Util
