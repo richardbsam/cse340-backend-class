8e48d265997b8d36c7e147144b0f51abcd5a0735
richardbsam <richardbonifacesam@gmail.com>
2024-10-26 05:33:16 +0100

    assignment04

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,6 +3,8 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/index") // Error-handling utility
+const { body } = require("express-validator");
+
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
@@ -13,26 +15,20 @@ router.get("/detail/:invId", utilities.handleErrors(invController.buildVehicleDe
 // Route to render the inventory management view
 router.get("/management", utilities.handleErrors(invController.buildManagementView))
 
-
-
-
-
 // Route to add a new classification (Task 2)
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassificationView));
+
 router.post("/add-classification", utilities.handleErrors(invController.addNewClassification));
+
 
 // Route to add a new inventory item (Task 3)
 router.get("/add-inventory", utilities.handleErrors(invController.addInventoryView));
-
-
-
 
 
 // Intentional Error Route for Task 3
 router.get("/trigger-error", utilities.handleErrors(async (req, res, next) => {
     throw new Error('Intentional server error triggered for testing!');
   }));
-
 
 
 module.exports = router;
