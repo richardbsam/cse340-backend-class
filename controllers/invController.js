8fa364648b27895a43546e0282360ec7c671fd06
richardbsam <richardbonifacesam@gmail.com>
2024-10-29 11:47:42 +0100

    Inventory Display

--- controllers/invController.js
+++ controllers/invController.js
@@ -44,10 +44,13 @@ invCont.buildVehicleDetail = async function (req, res, next) {
 invCont.buildManagementView = async function (req, res, next) {
   let nav = await utilities.getNav(); 
   const message = req.flash('message') || null;
+  let classificationSelect = await utilities.buildClassificationList();
+
   res.render("./inventory/management", {
     title: "Inventory Management",
     nav,
     message,
+    classificationSelect,
   });
 };
 
@@ -174,6 +177,18 @@ invCont.addNewVehicle = async function (req, res, next) {
   }
 };
 
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
 
 
 module.exports = invCont;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,21 +5,18 @@ const invController = require("../controllers/invController")
 const utilities = require("../utilities/index") // Error-handling utility
 const { body } = require("express-validator");
 
-
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 
 // Route to display the inventory item detail view based on the inventory ID
 router.get("/detail/:invId", utilities.handleErrors(invController.buildVehicleDetail))
 
+
 // Route to render the inventory management view
 router.get("/", utilities.handleErrors(invController.buildManagementView))
 
 // Route to add a new classification (Task 2)
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassificationView));
-
-
-
 
 // Route to process the add-classification form with validation
 router.post(
@@ -41,11 +38,14 @@ router.get("/add-inventory", utilities.handleErrors(invController.buildAddInvent
 // Route to handle form submission and add a new vehicle
 router.post("/add-inventory", utilities.handleErrors(invController.addNewVehicle));
 
+
+// Route to process the route and return the data as JSON
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
+
 // Intentional Error Route for Task 3
 router.get("/trigger-error", utilities.handleErrors(async (req, res, next) => {
   throw new Error('Intentional server error triggered for testing!');
 }));
-
 
 
 module.exports = router;
